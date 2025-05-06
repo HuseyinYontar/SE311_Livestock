3064414dee4e1044ab8abf046aa978da5d18a878
CDdev <cdsff549@gmail.com>
2025-05-06 22:29:19 +0300

    visitors

--- src/main/java/Cattle.java
+++ src/main/java/Cattle.java
@@ -1,11 +1,49 @@
 public abstract class Cattle {
-
+    protected int cattleId;
+    public abstract void accept(Visitor v);
+    public abstract boolean checkBarcode();
+    public abstract String checkHealth();
+    public int getCattleId(){
+        return cattleId;
+    }
 }
 
 class DairyCattle extends Cattle{
+    public void accept(Visitor v) {
+        v.visit(this);
+    }
+
+    @Override
+    public boolean checkBarcode() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkBarcode'");
+    }
+
+    @Override
+    public String checkHealth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkHealth'");
+    }
+
 
 }
 
 class BeefCattle extends Cattle{
+    public void accept(Visitor v) {
+        v.visit(this);
+    }
+
+    @Override
+    public boolean checkBarcode() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkBarcode'");
+    }
+
+    @Override
+    public String checkHealth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkHealth'");
+    }
+
 
 }
